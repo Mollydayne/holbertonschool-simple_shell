f118660eddc59962f9efa9e1175421404bb06236
Mollydayne <cl.w.perez@gmail.com>
2025-01-07 20:36:37 +0100

    Correcting a variable declaration and adding a description for userinput.c

--- userinput.c
+++ userinput.c
@@ -1,11 +1,26 @@
 #include "main.h"
 #include <errno.h>
 
+/**
+ * user_input - Reads a line of input from the user
+ *
+ * Description:
+ * This function prompts the user with a "$ " symbol and reads a line of
+ * text from standard input. The input is stored in a dynamically allocated
+ * buffer. If the input contains a newline character, it is replaced with
+ * a null terminator. In case of an error (e.g., EOF), the function frees
+ * the allocated memory and exits the program.
+ *
+ * Return: A pointer to the dynamically allocated string containing
+ * the user's input.
+ */
+
 char *user_input()
 {
 	char *buffer = NULL;
 	size_t bufferSize = 0;
 	int result = 0;
+	int i = 0;
 
 	printf("$ ");
 	fflush(stdout);
@@ -21,7 +36,7 @@ char *user_input()
 		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; buffer[i] != '\0'; i++)
+	for (i = 0; buffer[i] != '\0'; i++)
 	{
 		if (buffer[i] == '\n')
 		{
