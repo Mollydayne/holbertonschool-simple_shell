26b25a3d6dc63d7015fddc13bb8168a2aac1bc28
Mollydayne <cl.w.perez@gmail.com>
2025-01-07 18:46:35 +0100

    WIP on main.c for task 2. Simple shell 0.1

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "shell.h"
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Return: Always 0
+ * @argc: The number of arguments passed to the program
+ * @argv: The array of arguments passed to the program
+ *
+ * Description:
+ * This program continuously prompts the user for input,
+ * checks if the provided input corresponds to an accessible
+ * file path, and prints "Executable" if the file exists.
+ *
+ * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-printf("Hello u");
+	char *input;
+
+	while (1)
+	{
+		input = user_input();
+		if (access(input, F_OK) == 0)
+		{
+			printf("Executable\n");
+		}
+
+		free(input);
+	}
+
 	return (0);
 }
